b59bcd05d70fe93260e95d4bb27d0b4cdc8f92d3
guyschwartzberg <bojanvrucina@gmail.com>
2020-09-27 15:15:51 +0300

    lol

--- src/Map/index.js
+++ src/Map/index.js
@@ -1,17 +1,9 @@
 import React, {PureComponent} from 'react';
-import ReactMapGL, {Marker}  from 'react-map-gl'
-import { BiMap } from 'react-icons/bi'
+import ReactMapGL, { Popup }  from 'react-map-gl'
+import Pins from './pins'
 
 const accessToken = 'pk.eyJ1IjoiZGFtcmlkZXIiLCJhIjoiY2tmMng3ZGt0MDBhODJ5cG1rdzM1b3hvOCJ9.UdmG7TNnsdzMYK_S74GyNQ'
 
-class Markers extends PureComponent {
-    render() {
-      const {data} = this.props;
-      return data.length !== 0 ? data.map(
-        city => <Marker key={city.hebrewName} longitude={city.longitude} latitude={city.latitude} > <BiMap/> </Marker>
-      ) : null
-    }
-  }
   
   class Map extends PureComponent {
     constructor(props) {
@@ -23,8 +15,33 @@ class Markers extends PureComponent {
             zoom: 6.5,
             bearing: 0,
             pitch: 0
-          }
+          },
+          popupInfo: null
         };
+      }
+
+      _onClickMarker = city => {
+        this.setState({popupInfo: city});
+      }
+
+      _renderPopup() {
+        const {popupInfo} = this.state;
+        return (
+          popupInfo && (
+            <Popup
+              tipsize={5}
+              anchor="top"
+              longitude={popupInfo.longitude}
+              latitude={popupInfo.latitude}
+              closeonClick={false}
+              onClose={() => this.setState({popupInfo: null})}
+              >
+                {popupInfo.hebrewName}
+                <br/>
+                {"אוכלוסייה - " + popupInfo.population}
+              </Popup>
+          )
+        )
       }
     
   
@@ -38,7 +55,9 @@ class Markers extends PureComponent {
         width="1000px"
         mapStyle='mapbox://styles/damrider/ckfiqze0w0n1t19o7vzzbh5gr'
         >
-          { <Markers data={data} /> }
+          { <Pins data={data} onClick={this._onClickMarker}/> }
+
+          {this._renderPopup()}
         </ReactMapGL>
       );
     }
